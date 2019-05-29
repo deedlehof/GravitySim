c79fd55c4e0291a5751912817f7135bab80250d8
deedlehof <tannerco97@gmail.com>
2019-05-29 09:23:55 -0400

    Initial commit. Compiles and displays 'Hello World' in new window.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QLabel>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	QApplication app(argc, argv);
+	QLabel hello("Hello World");
+
+	hello.show();
+	return app.exec();
+}
